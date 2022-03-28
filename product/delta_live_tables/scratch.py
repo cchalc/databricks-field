d431bfe0949c0c55a785d78214829faa6c0ec5a2
cchalc <cchalc>
2022-03-28 08:56:22 +0000

    got dlt pipeline working

--- product/delta_live_tables/scratch.py
+++ product/delta_live_tables/scratch.py
@@ -41,59 +41,12 @@ assert " " not in ''.join(rules.columns)
 
 # COMMAND ----------
 
-rules.filter(col("tag").contains("validity"))
-
-# COMMAND ----------
-
-for row in rules.filter(col("tag") == "validity").collect():
-  print(row)
-
-# COMMAND ----------
-
-# dbutils.fs.rm(cloud_storage_path, True)
-
-# COMMAND ----------
-
 # MAGIC %run ../../_resources/setup
 
 # COMMAND ----------
 
-def get_rules(tag):
-  """
-    loads data quality rules from csv file
-    :param tag: tag to match
-    :return: dictionary of rules that matched the tag
-  """
-  rules = {}
-  df = spark.read.format("csv").option("header", "true").load("/FileStore/tables/christopher_chalcraft/rules.csv")
-  for row in df.filter(col("tag") == tag).collect():
-    rules[row['name']] = row['constraint']
-  return rules
+df.write.format("delta").mode("overwrite").option("path", f"{cloud_storage_path}/tables/farmers_market").saveAsTable("farmers_market")
 
 # COMMAND ----------
 
-expectation_rules = get_rules("validity")
 
-# COMMAND ----------
-
-(rules
- .coalesce(1)
- .write
- .format("csv")
- .mode("overwrite")
- .option("header", True)
- .option("path", f"{cloud_storage_path}/rules")
- .saveAsTable("rules")
-)
-
-# COMMAND ----------
-
-# MAGIC %fs ls /Users/christopher.chalcraft@databricks.com/field_demos/dltpoc/tables
-
-# COMMAND ----------
-
-df.write.format("delta").mode("overwrite").option("path", f"{cloud_storage_path}/tables/farmers_marke").saveAsTable("farmers_market")
-
-# COMMAND ----------
-
-# MAGIC %fs ls /FileStore/tables/christopher_chalcraft
